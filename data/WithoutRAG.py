7aef92cb692c4812e7093ca41ebf323efc9401c1
Joanna Stark <joanna.stark@hotmail.com>
2025-12-16 13:19:15 +0100

    changed name and modell

--- data/WithoutRAG.py
+++ data/WithoutRAG.py
@@ -4,15 +4,16 @@ from dotenv import load_dotenv
 
 # Run code with: docker compose exec app python data/main.py
 
+query = "Vilket är det vanligaste färdmedlet bland de med en sammanlagd hushållsinkomst under 10 000 kronor per månad? "
 
 load_dotenv()
 
 client = OpenAIClient(
     api_key=os.getenv("OPENAI_API_KEY"),
-    model="gpt-5",
+    model="gpt-4o-mini",
     system_prompt="You are a helpful assistant that can answer questions and help with tasks.",
 )
 
-response = client.invoke("Explain the concept of quantum computing in one sentence.")
+response = client.invoke(query)
 print(response.text)
 
